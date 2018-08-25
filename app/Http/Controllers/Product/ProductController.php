231a0a8bedf8598fac6b700309f313d587a6b3b1
Gordana <minjon56@gmail.com>
2018-08-25 10:17:48 +0200

    Added Crinsane shopping cart package and display cart functionality

--- app/Http/Controllers/Product/ProductController.php
+++ app/Http/Controllers/Product/ProductController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Product;
 
+use App\Http\Controllers\Controller;
 use App\Product;
 use Illuminate\Http\Request;
 
--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,12 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::resource('products', 'ProductController');
+Route::resource('products', 'Product\ProductController');
+
+Route::prefix('cart')->as('carts.')->group(function(){
+    Route::name('index')->get('/', 'Cart\CartController@index');
+    // Route::name('destroy')->delete('/', 'Cart\CartController@destroy');
+    // Route::name('store')->post('/{product}', 'Cart\CartController@store');
+    // Route::name('update')->put('/{rowId}', 'Cart\CartController@update');
+    // Route::name('delete')->delete('/{rowId}', 'Cart\CartController@removeProduct');
+});
